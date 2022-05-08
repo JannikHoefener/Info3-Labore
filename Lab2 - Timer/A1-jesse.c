0a512c64a61934084e4c623c7b86a0ba62ecb6dc
Soeren Helms <soeren.helms@repronik.it>
2022-05-08 12:26:02 +0000

    Update Lab2 - Timer/A1-jesse.c

--- Lab2 - Timer/A1-jesse.c
+++ Lab2 - Timer/A1-jesse.c
@@ -1,3 +1,10 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 04.05.2022 13:41:35
+ * Author : acx211
+ */ 
+
 #define F_CPU 16E6
 
 #include <avr/io.h>
@@ -5,55 +12,50 @@
 
 int main(void)
 {
-    init();
-    while(1){}
+	init();
+	while(1){}// While True ...
 }
 
 void init(void){
-	// Output: LED Bank
-    // DDR = Data Direction Register
-    //  0 = es ist ein Eingang
-    //  1 = es ist ein Ausgang
-    // dementsprechend definieren wir an den Positionen unserer LED Bank die LEDs als Ausgänge, den Rest als Eingänge
-	DDRB |= 0b1;
-	//    Pos 0
-	DDRC |= 0b111110;
-	//    Pos 543210
+	// LEDs als Output definieren
+	// DDRx - DataDirektionRegister 
+	// => gibt die Richtung an in welche Richtung die Daten fließen
+	//BitPos  76543210	
+	DDRB |=        0b1;
+	DDRC |=   0b111110; 
 	DDRD |= 0b11110000;
-	//    Pos 76543210
 
-	// Interrupt - see page 186 in documentation for more details about Timers
-	sei();								// Interrupts aktivieren
-	TIMSK0 |= (1<<OCIE0A); 				// Timer0 A Match enable
-	OCR0A = 155;						// 
-	TCCR0A |= (1<<WGM01); 				// Configure CTC Mode
-	TCCR0A &= ~(1<<WGM00);				// 
-	TCCR0B &= ~(1<<WGM02);				//
-	TCCR0B |=(1<<CS02) | (1<<CS00); 	// Prescaler 1024
-	TCCR0B &= ~(1<<CS01);				// 
+	// Interrupt (Timer = Interupt-Funktion)
+	sei(); // Interrupts generell aktivieren 
+	TIMSK0 |= (1<<OCIE0A); 
+	OCR0A = 155;
+	TCCR0A |= (1<<WGM01); 
+	TCCR0A &= ~(1<<WGM00);
+	TCCR0B &= ~(1<<WGM02);
+	TCCR0B |=(1<<CS02) | (1<<CS00); 
+	TCCR0B &= ~(1<<CS01);
 }
 
 ISR(TIMER0_COMPA_vect){
 	static volatile uint8_t counter = 0;
-	static volatile uint8_t position = 0;
+	static volatile uint8_t led_nr = 0;
 	counter++;
-	if (counter==100){					// 1 sec = 1000 ms, if 100 is required, 1 step should be 10 ms
-		counter = 0;					// reset the counter to wait for another 100 steps (1 sec)
-		// different handling, depending on position (in the light array)
-		if (position < 5) {				// 0 1 2 3 4
+	if (counter==100){
+		counter = 0;
+		if (led_nr < 5) {
 			PORTB &= ~(0b1);
 			PORTC &= ~(0b111110);
-			PORTC = (1 << (position + 1));
+			PORTC = (1 << (led_nr + 1));
 		}
-		else if(position < 9) {			// 5 6 7 8
+		else if(led_nr < 9) {
 			PORTC &= ~(0b111110);
 			PORTD &= ~(0b11110000);
-			PORTD = (1 << (position - 1));
-		} else {						// 9
+			PORTD = (1 << (led_nr - 1));
+			} else {
 			PORTD &= ~(0b11110000);
 			PORTB &= ~(0b1);
 			PORTB = 1;
 		}
-		position = (position + 1) % 10;		// makes sure, that position stays in range 0 to 9
+		led_nr = (led_nr + 1) % 10;
 	}
 }
